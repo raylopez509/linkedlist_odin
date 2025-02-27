01f786722bc4c7bfbd9b38e4a4b016fc2e4e7cc9
Ray Lopez <raylopez509@gmail.com>
2025-02-26 16:51:58 -0800

    added prepend and at functions

--- linkedlist.js
+++ linkedlist.js
@@ -7,6 +7,7 @@ class LinkedList {
     const newNode = new Node(value);
     if(this.head === null) {
       this.head = newNode;
+      this.tail = newNode;
     }
     else {
       let node = this.head;
@@ -14,8 +15,30 @@ class LinkedList {
         node = node.nextNode;
       }
       node.nextNode = newNode;
+      this.tail = newNode;
     }
     this.size++;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    if(this.head === null) {
+      this.head = newNode;
+    }
+    else {
+      let node = this.head;
+      newNode.nextNode = node;
+      this.head = newNode
+    }
+    this.size++;
+  }
+
+  at(index) {
+    let node = this.head
+    for(let i = 0; i < index; i++) {
+      node = this.head.nextNode;
+    } 
+    return node;
   }
 
   toString() {
@@ -46,4 +69,8 @@ class Node {
 const ll = new LinkedList();
 ll.append(1);
 ll.append(2);
+ll.prepend(0);
 console.log(ll.toString());
+console.log(ll.size);
+console.log(ll.at(0));
+console.log(ll.tail);
